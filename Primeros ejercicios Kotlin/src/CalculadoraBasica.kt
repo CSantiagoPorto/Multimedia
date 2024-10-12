cbd227feb4020a50f9eccafd8f177de85e8f0e46
CSantiagoPorto <148074839+CSantiagoPorto@users.noreply.github.com>
2024-10-12 09:33:37 +0200

    Calculadora máso menos funciona

--- Primeros ejercicios Kotlin/src/CalculadoraBasica.kt
+++ Primeros ejercicios Kotlin/src/CalculadoraBasica.kt
@@ -6,18 +6,32 @@ fun main(){
     var op2 = readln().toDouble()
     println("Qué operación quieres realizar?")
     var eleccion= readln().toInt()
-    while(eleccion!=1) {
+    do {
         println("1. Sumar")
         println("2. Restar")
         println("3. Multiplicar")
         println("4. Dividir")
         println("5. Salir")
-    }
 
-}
+    when (eleccion) {// Estructura de control equivalente al switch
+        1 -> println("El resultado es: ${sumar(op1, op2)}")
+        2 -> println("El resultado es: ${restar(op1, op2)}")
+        3 -> println("El resultado es: ${multiplicar(op1, op2)}")
+        4 -> {
+            if (op2 != 0.0) {//Hay que evitar que divida por 0 o peta
+                println("El resultado es: ${dividir(op1, op2)}")
+            } else {
+                println("No se puede dividir por cero")
+            }
+        }
+        5 -> println("Saliendo del programa...")
+        else -> println("Opción no válida, por favor elige una opción del menú.")
+    }
+}while(eleccion!=5)
 
 fun sumar(op1:Double, op2:Double):Double{
     var resultado = op1+op2
+    println("El resultado es $resultado")
     return resultado
 }
 fun restar(op1:Double, op2:Double):Double{
