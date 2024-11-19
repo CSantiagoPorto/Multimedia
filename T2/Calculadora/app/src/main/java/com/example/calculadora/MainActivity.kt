9126f548e48cd6d0d2e3b770f1105c8273befe59
CSantiagoPorto <148074839+CSantiagoPorto@users.noreply.github.com>
2024-11-19 13:57:51 +0100

    Se gira y mantiene la cifra

--- T2/Calculadora/app/src/main/java/com/example/calculadora/MainActivity.kt
+++ T2/Calculadora/app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.calculadora.databinding.ActivityMainBinding
+import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity(),OnClickListener {
     private lateinit var binding: ActivityMainBinding
@@ -17,13 +18,17 @@ class MainActivity : AppCompatActivity(),OnClickListener {
     private lateinit var operador:String
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        //savedInstanceState es una instancia del parámetro guardado
         super.onCreate(savedInstanceState)
         //Asocia lógico y gráfico
         binding=ActivityMainBinding.inflate(this.layoutInflater)//Activa al layout para poder usarlo
         setContentView(binding.root)
-
-       op2= savedInstanceState?.getInt("op2")?:0
-
+        //Lo gráfico tiene que ir siempre debajo de setContentView
+        op1 =savedInstanceState?.getInt("op1") ?: 0
+        op2= savedInstanceState?.getInt("op2")?:0
+        operador = savedInstanceState?.getString("operador") ?: ""
+        binding.textoInicio.text = savedInstanceState?.getString("textoInicio") ?: "0"
+        //Esta últmima línea restaura el valor al girar la pantalla. Si tenía valor lo restaura y si no es 0
 
         binding.btn0.setOnClickListener(this)//this es un objeto de la clase MainActivity
         binding.btn1.setOnClickListener(this)
@@ -35,7 +40,7 @@ class MainActivity : AppCompatActivity(),OnClickListener {
         binding.btn7.setOnClickListener(this)
         binding.btn8.setOnClickListener(this)
         binding.btn9.setOnClickListener(this)
-
+        binding.btnBorrar.setOnClickListener(this)
         binding.btnSumar.setOnClickListener(this)
         binding.btnRestar.setOnClickListener(this)
         binding.btnMultiplicar.setOnClickListener(this)
@@ -46,12 +51,16 @@ class MainActivity : AppCompatActivity(),OnClickListener {
 
 
 
-    override fun onSaveInstanceState(outState: Bundle) {
+    override fun onSaveInstanceState(outState: Bundle) {//outState es el parámetro de info saliente, mediante unobjeto de tipo Bundle
+        //Este método sirve para preservar las variables cuando se ejecuta el método destroy()
+        //Gracias al él podemos mantener la información de las variables al girar pantalla
         super.onSaveInstanceState(outState)
         //Esto es una cajita donde guardamos las variables
+
         outState.putInt ("op1", op1)
         outState.putInt("op2",op2)
         outState.putString("operador", operador)
+        outState.putString("textoInicio", binding.textoInicio.text.toString())
     }
     private fun añadirNumero(numero:String){
         if (binding.textoInicio.text.toString()=="0"){
@@ -64,7 +73,7 @@ class MainActivity : AppCompatActivity(),OnClickListener {
             //Es decir, quiero que ponga 35, no 035
         }
     }
-    override fun onClick(v: View) {
+    override fun onClick(v: View) {//v es quien provoca el evento
         when (v.id) {//Como hemos implementado la interfaz podemos usar el método onClick y ahorrar
             //líneas de código
             //Con R.id puedo acceder al botón del xml, y llamamos al método añadirNumero
@@ -78,7 +87,29 @@ class MainActivity : AppCompatActivity(),OnClickListener {
             R.id.btn7 -> añadirNumero("7")
             R.id.btn8 -> añadirNumero("8")
             R.id.btn9 -> añadirNumero("9")
+            R.id.btnSeno -> {
+                op1 = binding.textoInicio.text.toString().toIntOrNull() ?: 0
+                resultado = kotlin.math.sin(Math.toRadians(op1.toDouble())).toInt()
+                binding.textoInicio.text = resultado.toString()
+            }
+            R.id.btnCoseno -> {
+                op1 = binding.textoInicio.text.toString().toIntOrNull() ?: 0
+                resultado = kotlin.math.cos(Math.toRadians(op1.toDouble())).toInt()
+                binding.textoInicio.text = resultado.toString()
+            }
+            R.id.btnTangente -> {
 
+                val radianes = Math.toRadians(op1.toDouble())
+                resultado = kotlin.math.tan(radianes).roundToInt() // Calcula y aproxima porque ya tenía toda la calculadora hecha en Int
+                binding.textoInicio.text = resultado.toString() // Muestra el resultado aproximado
+
+                operador =""
+            }
+            R.id.btnCuadrado->{
+                op1=binding.textoInicio.text.toString().toIntOrNull() ?:0
+                resultado= op1*op1
+                binding.textoInicio.text= resultado.toString()
+            }
 
             R.id.btnSumar -> {
                 op1 = binding.textoInicio.text.toString().toInt()
@@ -109,11 +140,11 @@ class MainActivity : AppCompatActivity(),OnClickListener {
                     "÷" -> if (op2 != 0) {op1 / op2
                             }else {
                                  binding.textoInicio.text = "0"
-                                    return
+                        return
                     }
                     else -> 0// Si no se pulsa ninguno, volvemos a cero
                 }
-                binding.textoInicio.text = resultado.toString()
+                binding.textoInicio.text = resultado.toString() //Guardamos el resultado como String
                 operador =""
             }
             R.id.btnBorrar-> {
